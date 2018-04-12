16637c01b87876c41c1a9981ca5e46d171c9d5cc
DELL\guest1Day <kumeta22@gmail.com>
2018-04-12 11:55:12 +0900

    PHP 四則演算　課題提出します。

---
+++ CampChallenge_PHP/003.四則演算/sisokuennzann.php
@@ -0,0 +1,13 @@
+<?php
+const BASE = 100;
+$num = 5;
+
+$tasu = BASE + $num ;
+$hiku = BASE - $num ;
+$kake = BASE * $num ;
+$waru = BASE / $num ;
+
+echo " $tasu ";
+echo " $hiku ";
+echo " $kake ";
+echo " $waru ";
