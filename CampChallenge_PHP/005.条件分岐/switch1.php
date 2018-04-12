e21b90bc7ca4095180ff85caab09472f0ac737ab
DELL\guest1Day <kumeta22@gmail.com>
2018-04-12 17:28:36 +0900

    PHP　条件分岐　switch1 課題提出します。

---
+++ CampChallenge_PHP/005.条件分岐/switch1.php
@@ -0,0 +1,14 @@
+<?php
+$type = 2;
+
+switch($type) {
+    case 1:
+        echo 'one';
+        break;
+    case 2:
+        echo 'two';
+        break;
+    default:
+        echo '‘z’èŠO';
+        break;
+}
