514250e64c72540fbd13882ef079982b50cc3d05
DELL\guest1Day <kumeta22@gmail.com>
2018-04-12 17:29:58 +0900

    PHP　条件分岐　switch2　課題提出します。

---
+++ CampChallenge_PHP/005.条件分岐/switch2.php
@@ -0,0 +1,14 @@
+<?php
+$type = 'A';
+
+switch($type) {
+    case 'A':
+        echo '‰pŒê';
+        break;
+    case '‚ ':
+        echo '“ú–{Œê';
+        break;
+    default:
+        echo '<br>';
+        break;
+}
