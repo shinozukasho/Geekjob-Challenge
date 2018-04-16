3657c017f458bbde4b5d58edce35369cd808d480
DELL\guest1Day <kumeta22@gmail.com>
2018-04-16 13:55:11 +0900

    PHP　関数　引数　課題1提出します。

---
+++ CampChallenge_PHP/009.関数/hikisuu1.php
@@ -0,0 +1,14 @@
+<?php
+// “n‚³‚ê‚½”š‚ğŠï”‚©‹ô”‚©”»•Ê‚µ‚Ü‚·B
+// ‚½‚¾‚µA”š‚ª“n‚³‚ê‚È‚©‚Á‚½ê‡‚É‚ÍA1‚ğ”»•Ê‚µAŠï”‚Æ‚¢‚¢‚Ü‚·B
+function kisuu($num=1) {
+    if($num % '2'){
+    echo "odd";
+} 
+    else {
+    echo "even";
+}
+}
+kisuu(2);
+echo "<br>";
+kisuu(5);
