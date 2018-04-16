1e7720a5cc0a4b6aacd40b8b6bf6a41268b7d86f
DELL\guest1Day <kumeta22@gmail.com>
2018-04-16 11:57:16 +0900

    PHP 関数　ユーザー関数　課題提出します。

---
+++ CampChallenge_PHP/009.関数/localkansuu.php
@@ -0,0 +1,12 @@
+<?php
+
+// 自己紹介するユーザー定義関数
+function myprofile() {
+    echo "shinozuka<br>";
+    echo "1994/3/29<br>";
+    echo "I like sports.<br>";
+}
+
+for ($i = 0; $i < 10; $i++) {
+myprofile();
+}
