c1ae47b8a1ecd8258fb35a6102f9475327dc4bc5
DELL\guest1Day <kumeta22@gmail.com>
2018-05-10 14:19:11 +0900

    PHP DB操作　特定のレコードの更新　課題提出します。

---
+++ CampChallenge_PHP/013.DB操作/pdo10.php
@@ -0,0 +1,29 @@
+﻿<form action = "pdo10.php" method = "post">
+profilesID:<input type = "text" name = "profilesID">
+<input type = "submit">
+<?php
+//接続文字入力
+try {
+    $dns  = 'mysql:host=localhost;';
+    $dns .= 'dbname=Challenge_db;';
+    $dns .= 'charset=utf8';
+    //PDO オブジェクト作成
+    $pdo_obj = new PDO($dns,'shinozuka','20612061At');
+    //SQL文作成
+    $profilesID = var_dump($_POST);
+    echo '<br>';
+    $sql  = "UPDATE profiles SET name = "yamaguchi",tel = "090-9999-9999",age = 46,birthday = "1972-1-10" ";
+    $sql .= "WHERE profilesID = $profilesID"; 
+    $sql = "SELECT * FROM profiles WHERE profilesID = 1";
+    //SQL文をPDOオブジェクトに設定
+    $pdo_st =$pdo_obj -> prepare($sql);
+    //SQL文の実行
+    $pdo_st -> execute();
+    //画面に表示
+    $datas = $pdo_st -> fetchAll(PDO::FETCH_ASSOC);
+    var_dump($datas);
+    //DB切断
+    $pdo_obj = null;
+} catch (PDOException $pdo_ex) {
+    echo '失敗しました。' . $pdo_ex->getMessage();
+}
