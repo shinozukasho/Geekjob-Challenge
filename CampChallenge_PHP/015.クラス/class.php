1847ae6e13dc6d638f70260c87308cc6a27187d9
DELL\guest1Day <kumeta22@gmail.com>
2018-05-11 10:44:56 +0900

    PHP クラス　クラスの作成　課題提出します。

---
+++ CampChallenge_PHP/015.クラス/class.php
@@ -0,0 +1,16 @@
+<?php
+
+// ƒNƒ‰ƒX‚ÌéŒ¾
+class league{
+    public $yokohama = '';
+    public $hiroshima = '';
+
+    public function setleague($y, $h) {
+        $this->yokohama = $y;
+        $this->hiroshima = $h;
+    }
+    public function hyouji($y,$h){
+        echo $y;
+        echo $h;
+}
+}
